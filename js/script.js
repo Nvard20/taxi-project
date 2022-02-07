1e0fae5cbe3f2a0db8ad5b3f71d3d6eddee3af49
Nvard Asatryan <nvard.asatryan96@gmail.com>
2022-02-08 01:35:54 +0400

    all

--- js/script.js
+++ js/script.js
@@ -1,3 +1,28 @@
 $('.burger').click(function(){
     $('.mob-content').slideToggle()
 });
+////
+$(document).ready(function(){
+ 
+  
+    //Dynamic Next
+   $(".btn-next").on("click", function(){
+     nextStep = $("#" + $(this).parents(".slider-step").data("nextStep"));
+     $(this).parents(".slider-step").attr("data-anim","hide-to--left");
+     nextStep.attr("data-anim","show-from--right");
+   });
+   
+   //Dynamic Back
+   $(".btn-back").on("click", function(){
+     backTo = $("#" + $(this).parents(".slider-step").data("backTo"));
+     backTo.attr("data-anim","show-from--left")
+     $(".btn-next").on("click", function(){
+        nextStep = $("#" + $(this).parents(".slider-step").data("nextStep"));
+        $(this).parents(".slider-step").attr("data-anim","hide-to--left");
+        nextStep.attr("data-anim","show-from--right");
+      });
+     
+ 
+   });
+
+ });
